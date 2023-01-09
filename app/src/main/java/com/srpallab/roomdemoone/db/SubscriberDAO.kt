1e2d26216a66c32bb60a8963f1daea02c9363161
Shafiqur Rahman <pallab.alif@gmail.com>
2023-01-09 11:42:29 +0600

    Add: Queries for CURD.

---
+++ app/src/main/java/com/srpallab/roomdemoone/db/SubscriberDAO.kt
@@ -0,0 +1,29 @@
+package com.srpallab.roomdemoone.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SubscriberDAO {
+    // If data with same id found Conflict Replace will
+    // replace the row
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubscriber(subscriber: Subscriber) : Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers(): LiveData<List<Subscriber>>
+}

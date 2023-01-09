b22bba14d6019f713562c2d4dd519d7ee0ee2233
Shafiqur Rahman <pallab.alif@gmail.com>
2023-01-09 13:13:23 +0600

    FIX: Indentation Error.

--- app/src/main/java/com/srpallab/roomdemoone/db/SubscriberDatabase.kt
+++ app/src/main/java/com/srpallab/roomdemoone/db/SubscriberDatabase.kt
@@ -16,14 +16,14 @@ abstract class SubscriberDatabase : RoomDatabase() {
             fun getInstance(context: Context) : SubscriberDatabase {
                 synchronized(this) {
                     var instance = INSTANCE
-                        if (instance == null) {
-                            instance = Room.databaseBuilder(
-                                context.applicationContext,
-                                SubscriberDatabase::class.java,
-                                "subscriber_data_database"
-                            ).build()
-                            INSTANCE = instance
-                        }
+                    if (instance == null) {
+                        instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SubscriberDatabase::class.java,
+                            "subscriber_data_database"
+                        ).build()
+                        INSTANCE = instance
+                    }
                     return instance
                 }
             }
